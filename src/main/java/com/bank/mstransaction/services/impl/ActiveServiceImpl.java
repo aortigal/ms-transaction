364a517565410f9c945ee1abd302cc7a732f1656
Amado David Ortiz Gallardo <aortigal@emeal.nttdata.com>
2022-08-22 20:20:37 -0500

    update active

--- src/main/java/com/bank/mstransaction/services/impl/ActiveServiceImpl.java
+++ src/main/java/com/bank/mstransaction/services/impl/ActiveServiceImpl.java
@@ -2,7 +2,6 @@ package com.bank.mstransaction.services.impl;
 
 import com.bank.mstransaction.models.utils.ResponseActive;
 import com.bank.mstransaction.services.ActiveService;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.web.reactive.function.client.WebClient;
 import reactor.core.publisher.Mono;
@@ -11,8 +10,11 @@ import reactor.core.publisher.Mono;
 @Service
 public class ActiveServiceImpl implements ActiveService {
 
-    @Autowired
-    WebClient webClient;
+    private final WebClient webClient;
+
+    public ActiveServiceImpl(WebClient.Builder webClientBuilder){
+        this.webClient = webClientBuilder.baseUrl("http://localhost:8083").build();
+    }
 
     @Override
     public Mono<ResponseActive> findByCode(String id)
--- src/main/java/com/bank/mstransaction/services/impl/ClientServiceImpl.java
+++ src/main/java/com/bank/mstransaction/services/impl/ClientServiceImpl.java
@@ -2,7 +2,6 @@ package com.bank.mstransaction.services.impl;
 
 import com.bank.mstransaction.models.utils.ResponseClient;
 import com.bank.mstransaction.services.ClientService;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.web.reactive.function.client.WebClient;
 import reactor.core.publisher.Mono;
@@ -10,8 +9,11 @@ import reactor.core.publisher.Mono;
 @Service
 public class ClientServiceImpl implements ClientService {
 
-    @Autowired
-    WebClient webClient;
+    private final WebClient webClient;
+
+    public ClientServiceImpl(WebClient.Builder webClientBuilder){
+        this.webClient = webClientBuilder.baseUrl("http://localhost:8080").build();
+    }
 
     @Override
     public Mono<ResponseClient> findByCode(String id)
